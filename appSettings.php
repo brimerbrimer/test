670f95f0ef34dfaf92e59dfb92b4998e9981f022
Brian Stephens <brian@brimer.net>
2017-11-13 12:17:58 -0500

    Add files via upload

---
+++ appSettings.php
@@ -0,0 +1,13 @@
+<?php
+    // PROD
+    // define("DB_SERVER",     "");
+    // define("DB_USERNAME",   "");
+    // define("DB_PASSWORD",   "");
+    // define("DB_DBNAME",     "");
+
+    // DEV
+    define("DB_SERVER",     "");
+    define("DB_USERNAME",   "");
+    define("DB_PASSWORD",   "");
+    define("DB_DBNAME",     "");
+?>
